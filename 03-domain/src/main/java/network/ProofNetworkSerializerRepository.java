88eac4395ae4b4bdb96e5d1869a7c578a925104f
i518183 <julien.meier@sap.com>
2022-03-29 09:44:25 +0200

    Project structure.

--- 03-domain/src/main/java/network/ProofNetworkSerializerRepository.java
+++ 03-domain/src/main/java/network/ProofNetworkSerializerRepository.java
@@ -3,6 +3,5 @@ package network;
 public interface ProofNetworkSerializerRepository {
     void setSuffix(String suffix);
     void serialize(ProofNetworkRepository proofNetwork);
-    void deserialize();
 }
 
